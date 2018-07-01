7fc3ea78218ad012194d6acda96625d7359b4d04
czero1995 <czero1995@gmail.com>
2018-07-01 15:58:38 +0800

    upload service

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,5 +1,5 @@
 <style lang="less">
-    @import './login.less';
+@import "./login.less";
 </style>
 
 <template>
@@ -30,7 +30,6 @@
                             <Button @click="handleSubmit" type="primary" long>登录</Button>
                         </FormItem>
                     </Form>
-                    <p class="login-tip">输入任意用户名和密码即可</p>
                 </div>
             </Card>
         </div>
@@ -38,46 +37,51 @@
 </template>
 
 <script>
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 export default {
-    data () {
-        return {
-            form: {
-                userName: 'iview_admin',
-                password: ''
-            },
-            rules: {
-                userName: [
-                    { required: true, message: '账号不能为空', trigger: 'blur' }
-                ],
-                password: [
-                    { required: true, message: '密码不能为空', trigger: 'blur' }
-                ]
-            }
-        };
-    },
-    methods: {
-        handleSubmit () {
-            this.$refs.loginForm.validate((valid) => {
-                if (valid) {
-                    Cookies.set('user', this.form.userName);
-                    Cookies.set('password', this.form.password);
-                    this.$store.commit('setAvator', 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg');
-                    if (this.form.userName === 'iview_admin') {
-                        Cookies.set('access', 0);
-                    } else {
-                        Cookies.set('access', 1);
-                    }
-                    this.$router.push({
-                        name: 'home_index'
-                    });
-                }
+  data() {
+    return {
+      form: {
+        userName: "",
+        password: ""
+      },
+      rules: {
+        userName: [
+          { required: true, message: "账号不能为空", trigger: "blur" }
+        ],
+        password: [{ required: true, message: "密码不能为空", trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          Cookies.set("user", this.form.userName);
+          Cookies.set("password", this.form.password);
+          this.$store.commit(
+            "setAvator",
+            "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg"
+          );
+          if (this.form.userName === "iview_admin") {
+            Cookies.set("access", 0);
+          } else {
+            Cookies.set("access", 1);
+          }
+          if (
+            this.form.userName == "inifite" &&
+            this.form.password == "inifite"
+          ) {
+            this.$router.push({
+              name: "home_index"
             });
+          }
         }
+      });
     }
+  }
 };
 </script>
 
 <style>
-
 </style>
